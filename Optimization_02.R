223972553d358da27c8287918be1bb7846b68d0a
Ketan Polawar <ketanpolawar@gmail.com>
2024-03-16 23:27:01 +0530

    Add files via upload

---
+++ Optimization_02.R
@@ -0,0 +1,18 @@
+#Constraints Optimazations 
+#Unconstraint Optimization : No boundries or soft Boundaries
+#Constraint Optimaxation : hard limit placed on value of decision variable that might lead to choose or eliminate certian direction
+ 
+#Equality constraints
+#Langrage Multipliers :
+# 1. It is a technique used in optimization problems that involve constraints
+# 2. For the objective function f(x,y) with constraint function g(x,y)
+# grad f(x,y)=-lambda grad g(x,y)
+#multivariate  inequality constraints optimization
+#constraints of type >= or <= 
+# lagrangian 
+# L(x1,x2...,u1,u2...)=equation +u(f<=0)+...
+# first oder KKT conditions 
+# u(f(x))=0
+# n euation for with derivative wrt to each variable =0
+#ui>=0
+
